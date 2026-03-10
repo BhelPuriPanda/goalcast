297030403bd808ffcfde534aeaceb07f1ea1ba73
BhelPuriPanda <boreover582@gmail.com>
2026-03-10 14:58:00 +0530

    Serer strted

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+});
